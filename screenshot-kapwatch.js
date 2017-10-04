9cce2b433bd32a796829d977f71e6b900f422098
Josh Begley <josh@joshbegley.com>
2017-10-04 17:55:32 -0400

    added puppeteer script

---
+++ screenshot-kapwatch.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  page.setViewport({width: 1024, height: 700});
+  await page.goto('https://kapwatch.com');
+  await page.waitFor(5000);
+  await page.screenshot({path: '/home/kapwatch/kapwatch-now.png'});
+
+  await browser.close();
+})();
